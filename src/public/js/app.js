a74d50e64ff1d172c309b7e0100828b8affe525e
coseo12 <momo_12@naver.com>
2021-08-16 14:05:08 +0900

    recap

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,12 +4,14 @@ const welcome = document.querySelector('#welcome');
 const form = welcome.querySelector('form');
 const input = form.querySelector('input');
 
+function backendDone(msg) {
+  console.log('Backend from message: ', msg);
+}
+
 function handleRoomSubmit(event) {
   event.preventDefault();
   const input = form.querySelector('input');
-  socket.emit('enter_room', { payload: input.value }, () =>
-    console.log('server is done!')
-  );
+  socket.emit('enter_room', { payload: input.value }, backendDone);
   input.value = '';
 }
 
